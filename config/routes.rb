ce072e7119f66ced2f9d1e522a86f95f2e9eec66
Jacqueline Lee <anginblu@gmail.com>
2018-01-29 18:37:04 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   resources :songs, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+=======
+  resources :songs, only: [:index, :show, :edit, :new, :create, :update]
+>>>>>>> 67347089cf4e9fa718af3d8b56eaf0680ca0d263
 end
